d451149e03ed9cb29948f78d3eefabb7d51d3220
balakondareddy <bannapureddy@innominds.com>
2019-07-04 13:01:56 +0530

    updated the selenium and testng testcases

---
+++ src/main/java/com/TestCalculation2.java
@@ -0,0 +1,52 @@
+package com;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Parameters;
+
+public class TestCalculation2 {
+	
+	@Test
+	public void testUnmatches() {
+		boolean contentsAreSame =  "Hyderabad".equals("Bangalore");
+		Assert.assertFalse(contentsAreSame, "String contents are not same");
+	}
+
+	@Test
+	public void calculateInterest() {
+		int interest = 100000*12*1/100;
+		Assert.assertEquals(interest, 1200, "interest calculation should be correct");
+	}
+	
+	@Test
+	public void additionOfTwoNumbers() {
+	    int y = 125+75;
+	    Assert.assertEquals(200, 200);
+	}
+	
+	@Test
+	public void subtractionOfTwoNumbers() {
+	    int y = 125-75;
+	    Assert.assertEquals(50, 50);
+	}
+	
+	@Test
+	public void testMatches() {
+		Assert.assertEquals("Chennai", "Chennai", "String contents are same");
+	}
+	
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		
+	}
+
+	@AfterTest
+	public void afterTest() {
+		
+	}
+
+}
---
+++ src/main/java/com/TestCalculation3.java
@@ -0,0 +1,52 @@
+package com;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Parameters;
+
+public class TestCalculation3 {
+	
+	@Test
+	public void testUnmatches() {
+		boolean contentsAreSame =  "Hyderabad".equals("Bangalore");
+		Assert.assertFalse(contentsAreSame, "String contents are not same");
+	}
+
+	@Test
+	public void calculateInterest() {
+		int interest = 10000*12*1/100;
+		Assert.assertEquals(interest, 1200);
+	}
+	
+	@Test
+	public void additionOfTwoNumbers() {
+	    int y = 1250+75;
+	    Assert.assertEquals(y, 200 , "addition of two numbers should be correct");
+	}
+	
+	@Test
+	public void subtractionOfTwoNumbers() {
+	    int y = 125-75;
+	    Assert.assertEquals(50, 50);
+	}
+	
+	@Test
+	public void testMatches() {
+		Assert.assertEquals("Bangalore", "Bangalore", "String contents are same");
+	}
+	
+	@BeforeTest
+	public void beforeTest() throws Exception {
+		
+	}
+
+	@AfterTest
+	public void afterTest() {
+		
+	}
+
+}
